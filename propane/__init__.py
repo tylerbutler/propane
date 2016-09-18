8be2454df1d651421c4de1e1ff5ff2b8ddda8e55
Tyler Butler <tyler@tylerbutler.com>
2016-09-18 13:13:23 -0700

    Fix import error.

--- propane/__init__.py
+++ propane/__init__.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 from __future__ import absolute_import, print_function
 
-from setuptools_scm import get_version
-
-
 __author__ = 'Tyler Butler <tyler@tylerbutler.com>'
 
-version = get_version(root='..', relative_to=__file__)
+
+def version():
+    from setuptools_scm import get_version
+    return get_version(root='..', relative_to=__file__)
