e52208f44baa9b653d6d90d10d817fe047ad910d
Tyler Butler <tyler@tylerbutler.com>
2016-09-18 12:02:07 -0700

    Minor formatting tweak.

--- propane/datetime.py
+++ propane/datetime.py
@@ -10,6 +10,7 @@ __author__ = 'Tyler Butler <tyler@tylerbutler.com>'
 def timestamp_to_ticks(t=None):
     """
     Convert a Python time into Ticks, equivalent to .NET's DateTime.Ticks value.
+
     :param t: the time to convert
     :return: the time in ticks
     """
